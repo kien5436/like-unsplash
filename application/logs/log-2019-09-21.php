7cf8350e9dd908db364c08bdc648d4905299c22a
phamkien <kiendp00@gmail.com>
2019-09-21 22:01:10 +0700

    change session config

--- application/logs/log-2019-09-21.php
+++ application/logs/log-2019-09-21.php
@@ -7,3 +7,11 @@ LINE 2: FROM "tags"
              ^ - Invalid query: SELECT "tag_name"
 FROM "tags"
  LIMIT 20
+ERROR - 2019-09-21 21:35:44 --> Severity: Notice --> Undefined offset: 0 /var/www/like-unsplash/application/models/SecureModel.php 19
+ERROR - 2019-09-21 21:38:24 --> Severity: Notice --> Undefined offset: 0 /var/www/like-unsplash/application/models/SecureModel.php 19
+ERROR - 2019-09-21 21:40:29 --> Severity: Notice --> Undefined offset: 0 /var/www/like-unsplash/application/models/SecureModel.php 19
+ERROR - 2019-09-21 21:40:38 --> Severity: Notice --> Undefined offset: 0 /var/www/like-unsplash/application/models/SecureModel.php 19
+ERROR - 2019-09-21 21:41:01 --> Severity: Notice --> Undefined offset: 0 /var/www/like-unsplash/application/models/SecureModel.php 19
+ERROR - 2019-09-21 21:41:55 --> 404 Page Not Found: Vendor/js
+ERROR - 2019-09-21 21:44:38 --> 404 Page Not Found: Vendor/js
+ERROR - 2019-09-21 21:44:40 --> 404 Page Not Found: Vendor/js
