58407bab83a8ca8df3e01005ee0f99ada92787e0
phamkien <kiendp00@gmail.com>
2019-09-22 09:22:25 +0700

    add composer

---
+++ application/logs/log-2019-09-22.php
@@ -0,0 +1,8 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2019-09-22 09:12:15 --> 404 Page Not Found: Faviconico/index
+ERROR - 2019-09-22 09:13:15 --> 404 Page Not Found: Vendor/js
+ERROR - 2019-09-22 09:13:15 --> 404 Page Not Found: Vendor/js
+ERROR - 2019-09-22 09:13:18 --> 404 Page Not Found: Vendor/js
+ERROR - 2019-09-22 09:14:42 --> 404 Page Not Found: Vendor/js
+ERROR - 2019-09-22 09:14:46 --> 404 Page Not Found: Vendor/js
